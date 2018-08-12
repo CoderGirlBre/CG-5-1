ee7dd01e85ed7d3a2afaeb96a08f25a94f9a68f5
CoderGirlBre <41300229+CoderGirlBre@users.noreply.github.com>
2018-08-12 14:15:43 -0500

    Add files via upload

--- FullName/Program.cs
+++ FullName/Program.cs
@@ -4,9 +4,33 @@ namespace FullName
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Asks the user for their first name and assigns it to firstName variable.
+            Console.Write("What's your first name? ");
+            string firstName = Console.ReadLine();
+
+            //Asks user their last name and assigns it to lastName variable.
+            Console.Write("What's your last name? ");
+            string lastName = Console.ReadLine();
+
+            //Prints "Your name backwards: " with the user's name spelled backwards on the same line.
+            Console.Write("Your name backwards: ");
+            ReverseString(firstName);
+            ReverseString(lastName);
+
+            Console.ReadLine();
+        }
+        //New method created to properly reverse the user's name.
+        private static void ReverseString(string message)
+        {//Copies each character in the message array (first and last name) and reverses it, then writes it to the console.
+            char[] messageArray = message.ToCharArray();
+            Array.Reverse(messageArray);
+            foreach (char item in messageArray)
+            {
+                Console.Write(item);
+            }
+            Console.Write(" ");
         }
     }
 }
